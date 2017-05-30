abe1bdef65b521d94f3af1674b66500fa8219ff2
newmang <1722073409@qq.com>
2017-05-30 15:55:41 +0800

    filterInfo.vue

--- src/components/filterInfo.vue
+++ src/components/filterInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="filter-drop-down">
-    <div class="filter-column">
+    <div class="filter-column" v-for="items in item">
     <el-dropdown>
   <span class="el-dropdown-link">
     下拉菜单<i class="el-icon-caret-bottom el-icon--right"></i>
@@ -8,15 +8,9 @@
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item>黄金糕</el-dropdown-item>
         <el-dropdown-item>狮子头</el-dropdown-item>
-        <el-dropdown-item>螺蛳粉</el-dropdown-item>
-        <el-dropdown-item disabled>双皮奶</el-dropdown-item>
-        <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
     </div>
-
-
-
   </div>
 </template>
 <style lang="scss" scoped>
