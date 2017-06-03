c730a89702bc577c67ff49b212387f15e3a3bae7
newmang <1722073409@qq.com>
2017-06-03 16:04:53 +0800

    Publishbuy.vue

--- src/page/Publishbuy.vue
+++ src/page/Publishbuy.vue
@@ -5,17 +5,17 @@
         <mt-button icon="back"></mt-button>
       </router-link>
     </mt-header>
-   <!--<publishPhoto></publishPhoto>-->
+   <publishPhoto></publishPhoto>
   </div>
 </template>
-<!--<script>-->
-<!--import publishPhoto from '../components/Publishphoto.vue'-->
-<!--export default {-->
-  <!--components: {-->
-    <!--publishPhoto: publishPhoto-->
-  <!--}-->
-<!--}-->
-<!--</script>-->
+<script>
+import publishPhoto from '../components/Publishphoto.vue'
+export default {
+  components: {
+    publishPhoto: publishPhoto
+  }
+}
+</script>
 <style lang="scss" scoped>
 .mint-header{height: 7rem;line-height: 7rem;background: #f6f6f6;color:#666666;font-size: 2rem;}
 .mintui{font-size: 2rem;}
