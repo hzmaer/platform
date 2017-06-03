a0a44b495e03cc22cd7559599c9336a5cb81c9a6
newmang <1722073409@qq.com>
2017-06-03 21:24:42 +0800

    publishForm

---
+++ src/components/province.vue
@@ -0,0 +1,73 @@
+<template>
+<nav class="location-bar">
+  <ul class="location-tree" v-for="item in items">
+    <li v-if="!item.children"><a href="#">{{item.text}}</a></li>
+    <li v-if="item.children"><a href="#" v-on:click="toggleChildren(item)" v-bind:class="{active:item.isActive}">{{item.text}}<span></span></a>
+      <ul v-show="item.isShow" class="childs">
+        <li v-for="child in item.children"><a href="#">{{child.text}}</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+</template>
+<script>
+export default{
+  methods: {
+    toggleChildren: function (item) {
+      item.isShow = !item.isShow
+      item.isActive = !item.isActive
+    }
+  },
+  data () {
+    return {
+      items: [
+        {
+          text: '湖北',
+          isShow: false,
+          isActive: true,
+          children: [{
+            text: '武汉'
+          }, {
+            text: '荆门'
+          }, {
+            text: '荆州'
+          }]
+        }, {
+          text: '浙江',
+          isShow: false,
+          isActive: true,
+          children: [{
+            text: '杭州'
+          }, {
+            text: '宁波'
+          }, {
+            text: '嘉兴'
+          }]
+        }, {
+          text: '广东',
+          isShow: false,
+          isActive: true,
+          children: [{
+            text: '深圳'
+          }, {
+            text: '广州'
+          }, {
+            text: '惠州'
+          }]
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.location-bar{position:absolute;top:0rem;left:0rem;width:100%;}
+.location-tree{
+ li{height: 4rem;line-height: 4rem;font-size: 2rem;background: #f6f6f6;
+ a{color:#666666;display: inline-block;width:100%;}
+ .childs{position: fixed;left:5rem;top:7rem;width:100%;
+ li{background: #ffffff;}
+ }
+}
+}
+</style>
---
+++ src/components/publishForm.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="publishformBox">
+  <div class="publishForm">
+    <a class="mint-cell">
+      <div class="mint-cell-wrapper">
+        <div class="mint-cell-title"><span class="mint-cell-text">省份</span></div>
+        <div class="mint-cell-value"><input type="text" placeholder="全国" id="province-value" class="mint-cell-val"></div>
+        <a class="mint-cell-more">></a>
+      </div>
+    </a>
+    <a class="mint-cell">
+      <div class="mint-cell-wrapper">
+        <div class="mint-cell-title"><span class="mint-cell-text">人手</span></div>
+        <div class="mint-cell-value"><input type="text" placeholder="2" id="num" class="mint-cell-val"><p>人</p></div>
+        <a class="mint-cell-more">></a>
+      </div>
+    </a>
+    <a class="mint-cell">
+      <div class="mint-cell-wrapper">
+        <div class="mint-cell-title"><span class="mint-cell-text">年利润</span></div>
+        <div class="mint-cell-value"><input type="text" placeholder="20-25" id="profit" class="mint-cell-val"><p>万/年</p></div>
+        <a class="mint-cell-more">></a>
+      </div>
+    </a>
+    <a class="mint-cell">
+      <div class="mint-cell-wrapper">
+        <div class="mint-cell-title"><span class="mint-cell-text">卖价</span></div>
+        <div class="mint-cell-value"><input type="text" placeholder="15" id="sell-price" class="mint-cell-val"><p>万</p></div>
+        <a class="mint-cell-more">></a>
+      </div>
+    </a>
+  </div>
+  <province></province>
+</div>
+</template>
+<script>
+import province from '../components/province.vue'
+export default {
+  components: {
+    province: province
+  }
+}
+</script>
+<style lang="scss" scoped>
+.publishformBox{position: relative;}
+.publishForm{padding: 1.5rem 1.5rem;
+.mint-cell{border-bottom: 1px solid #d4d4d4;margin:1rem 0rem;}
+.mint-cell-text{font-size: 2rem;color:#666666;}
+.mint-cell-more{-webkit-box-flex:1;-ms-flex:1;flex:1;text-align: right;color:#666666;}
+.mint-cell-title{-webkit-box-flex:2;-ms-flex:2;flex:2;}
+.mint-cell-value{-webkit-box-flex:5;-ms-flex:5;flex:5;font-size: 2rem;color:#666666;}
+.mint-cell-val{border: none;width:20%;}
+}
+
+</style>
